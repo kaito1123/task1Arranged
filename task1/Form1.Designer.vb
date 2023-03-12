555174bb35034115a348278d0ee0cb0493d2d5a1
kaito1123 <78860399+kaito1123@users.noreply.github.com>
2023-03-12 20:58:06 +0900

    Add files via upload

---
+++ task1/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(243, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "もとの言葉（3文字以上）"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(50, 119)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(483, 658)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(320, 20)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(304, 48)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "たぬき言葉にできるか判定" & Global.Microsoft.VisualBasic.ChrW(9)
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(581, 119)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(130, 40)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "生成 >>"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(581, 217)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(130, 40)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "<< 解読"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(753, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 24)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "たぬき言葉"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(769, 119)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(483, 658)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(581, 533)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(130, 31)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(556, 337)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(181, 144)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "「た」が入る間隔を" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "アレンジしよう！" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "好きな数字を" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "入れてください" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "数字を入れたまま" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "にして下さい。" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(581, 737)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(130, 31)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(556, 596)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(179, 120)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "「た」を好きな" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ひらがなに" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "変えてみよう！" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "文字は入れたまま" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "にして下さい。" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1367, 887)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ task1/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+
+    Dim createdText As String
+    Dim originalText As String
+    Dim tanukiNumber As Integer = 3
+    Dim favoriteWord As String = "た"
+
+    '文字が入る間隔を調整する処理
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        If Me.TextBox3.Text = String.Empty Then
+            tanukiNumber = 3
+        End If
+
+        If Int32.TryParse(Me.TextBox3.Text, tanukiNumber) Then
+            tanukiNumber = CInt(Me.TextBox3.Text)
+        Else
+            tanukiNumber = 3
+        End If
+
+        Me.Label1.Text = $"もとの言葉（{tanukiNumber}文字以上）"
+
+
+    End Sub
+
+    '暗号の文字変更についての処理
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        If Me.TextBox3.Text = String.Empty Then
+            favoriteWord = "た"
+        End If
+
+        favoriteWord = Me.TextBox4.Text
+
+        Me.Button1.Text = $"{favoriteWord}ぬき言葉にできるか判定"
+        Me.Label2.Text = $"{favoriteWord}ぬき言葉"
+
+    End Sub
+
+    '生成	文章をたぬき言葉に変換する
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        originalText = Trim(Me.TextBox1.Text)
+        originalText = originalText.Replace(Environment.NewLine, "")
+        Dim len As Integer = originalText.Length
+        If len < tanukiNumber Then
+            MessageBox.Show($"もとの言葉の文字数が足りないため、{favoriteWord}ぬき言葉にできません。",
+                            "情報",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+            Return
+        End If
+
+        If originalText.Contains(favoriteWord) Then
+            MessageBox.Show($"もとの言葉に「{favoriteWord}」が含まれているため生成できません。	",
+                            "情報",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information
+                            )
+            Return
+        End If
+
+
+        For i As Integer = 0 To originalText.Length - 1
+            createdText += originalText(i)
+
+            If ((i + 1) Mod tanukiNumber) = 0 And Not i = 0 Then
+                createdText += favoriteWord
+            End If
+
+        Next
+
+        Me.TextBox2.Text = createdText
+
+        originalText = ""
+
+        TextBox1.Clear()
+
+
+
+    End Sub
+
+
+    '解読	たぬき言葉をもとの言葉に変換する
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim changedText As String = Me.TextBox2.Text
+        If changedText = "" Then
+            MessageBox.Show($"{favoriteWord}ぬき言葉が入力されていません。",
+                            "情報",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+        End If
+
+        Dim revertText As String = ""
+        For i As Integer = 0 To changedText.Length - 1
+            If Not changedText(i) = favoriteWord Then
+                revertText += changedText(i)
+            End If
+        Next
+
+        Me.TextBox1.Text = revertText
+
+        createdText = String.Empty
+
+    End Sub
+
+
+    '判定	文章をたぬき言葉に変換できるか判定する
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        originalText = Trim(Me.TextBox1.Text)
+        originalText = originalText.Replace(Environment.NewLine, "")
+        Dim len As Integer = originalText.Length
+        If len < tanukiNumber Then
+            MessageBox.Show($"もとの言葉の文字数が足りないため、{favoriteWord}ぬき言葉にできません。",
+                            "情報",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information
+                            )
+            Return
+        End If
+
+        If originalText.Contains(favoriteWord) Then
+            MessageBox.Show($"もとの言葉に「{favoriteWord}」が含まれているため生成できません。	",
+                            "情報",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information
+                            )
+        Else
+            MessageBox.Show($"{favoriteWord}ぬき言葉にできます。",
+                        "情報",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                        )
+
+        End If
+
+    End Sub
+
+
+End Class
